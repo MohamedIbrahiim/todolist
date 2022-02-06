097aa375218026972de07a725600b19cd82933fe
Mohamed Ibrahem <emohamed250@gmail.com>
2022-02-06 14:08:36 +0200

    add register api and success unittest

--- app/serilaizers/__init__.py
+++ app/serilaizers/__init__.py
@@ -4,3 +4,4 @@ so we use : from .serializer import YourSerializer
 better than using : from .serializer.YourFileName import YourSerializer
 """
 from .todo import ToDoListSerializer
+from .auth import RegisterUserSerializer
--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class RegisterTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user_dict = {
+                "username": "test@username.com",
+                "email": "test@mail.com",
+                "first_name": "Test",
+                "last_name": "end-user",
+                "password": "12345678"
+            }
+
+    def test_200_ok(self) -> None:
+        response = self.client.post(
+            reverse("mobile:register"),
+            self.user_dict
+        )
+        user = User.objects.get(username="test@username.com")
+        assert response.status_code == 201
+        assert User.objects.count() == 1
+        assert user.email == "test@mail.com"
